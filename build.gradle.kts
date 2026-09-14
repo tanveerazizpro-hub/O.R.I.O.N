d5b52830f442d7a82c50289de12dba8e2effc812
tanveerazizpro-hub <tanveerazizpro@gmail.com>
2026-09-14 21:43:03 +0530

    Create build.gradle.kts with plugin versions
    Add root build file with plugin declarations for Android and Kotlin.

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+// Root build file. Plugin versions declared here apply to all modules.
+plugins {
+    id("com.android.application") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.20" apply false
+}
